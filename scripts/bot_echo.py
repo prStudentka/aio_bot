cb858efc8400893265c018a7ad8b08b59017f298
prStudentka <bk_nvk@mail.ru>
2024-05-12 16:27:03 +0300

    refactor start point

--- scripts/bot_echo.py
+++ scripts/bot_echo.py
@@ -2,12 +2,20 @@
 from dotenv import load_dotenv
 from os import getenv
 from aiogram import Bot
-from echo_bot import DISPATCHER
+from aiogram import Dispatcher
+from bot import echo_bot, game_bot
 
 
 load_dotenv()
 mybot = Bot(token=getenv('TOKEN'))
+DISPATCHER = Dispatcher()
+
+
+def main():
+    DISPATCHER.include_router(echo_bot.router)
+    DISPATCHER.include_router(game_bot.router_game)
 
 
 if __name__ == '__main__':
+    main()
     DISPATCHER.run_polling(mybot)
